325b65d7f4135680edc5e15dcb93966fcdca52e0
bungarahulreddy <rahulreddybunga@gmail.com>
2025-07-27 23:10:28 +0530

    Add files via upload

---
+++ Sample_concept_based_problems/Result_of_expressions.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class Result_of_expressions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int x,y,z;
+     x=10 + 2 * 5;
+     y=(10 + 2) * 5;
+     z=100 / 5 + 2 * 3 - 4;
+    System.out.println("Value of 10 + 2 * 5 = "+x);
+    System.out.println("Value of (10 + 2) * 5 = "+y);
+    System.out.println("Value of 100 / 5 + 2 * 3 - 4 = "+z);
+	}
+
+}
